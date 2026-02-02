3953d363cdd3bb5186dca332ac9e8ac3f045229e
grassblock <i@gb0.dev>
2026-02-02 14:49:06 +0800

    chore(test): empty the ssh agent socket to prevent reusing from agent

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-SSH_ASKPASS_REQUIRE=force SSH_ASKPASS=$SCRIPT_DIR/rbw-ssh-askpass ssh -o UserKnownHostsFile=/dev/null "$@"
+SSH_ASKPASS_REQUIRE=force SSH_ASKPASS=$SCRIPT_DIR/rbw-ssh-askpass SSH_AUTH_SOCK="" ssh -o UserKnownHostsFile=/dev/null "$@"
